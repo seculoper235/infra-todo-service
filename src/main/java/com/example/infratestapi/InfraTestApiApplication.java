4bbca9da5aa3593881bf987b936904e9303f92b5
seculoper <mir8725@naver.com>
2025-01-23 19:17:48 +0900

    Initial commit

---
+++ src/main/java/com/example/infratestapi/InfraTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.infratestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraTestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfraTestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/infratestapi/InfraTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infratestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraTestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
